5f9fd4206d5ca9f501e0d303f3c2ce3bb4cd9d78
younes-karir <youneskarir@outlook.com>
2024-03-01 16:37:55 +0100

    initial commit

---
+++ src/main/java/com/youneskarir/springbootjpamanytomanycrud/SpringBootJpaManyToManyCrudApplication.java
@@ -0,0 +1,13 @@
+package com.youneskarir.springbootjpamanytomanycrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaManyToManyCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJpaManyToManyCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/youneskarir/springbootjpamanytomanycrud/SpringBootJpaManyToManyCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youneskarir.springbootjpamanytomanycrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJpaManyToManyCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
